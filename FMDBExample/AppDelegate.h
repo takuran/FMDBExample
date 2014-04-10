f31a5e5a8b8af518bd3b0fff33ae00e4f2028304
Naoyuki Takura <takuran@gmail.com>
2014-04-11 00:13:25 +0900

    Initial Commit

---
+++ FMDBExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FMDBExample
+//
+//  Created by Naoyuki Takura on 2014/04/11.
+//  Copyright (c) 2014年 Naoyuki Takura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FMDBExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FMDBExample
+//
+//  Created by Naoyuki Takura on 2014/04/11.
+//  Copyright (c) 2014年 Naoyuki Takura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
